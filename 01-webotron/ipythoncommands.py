542a92f25d938bfb2bc8edbbcbc0b2f9f602599f
mahesh0414 <bhatt.mahesh98@yahoo.com>
2018-08-24 20:19:30 -0500

    Update ipythoncommands.py

--- 01-webotron/ipythoncommands.py
+++ 01-webotron/ipythoncommands.py
@@ -1,19 +1,12 @@
 # coding: utf-8
 import boto3
 session = boto3.Session(profile_name='pythonAutomation')
-s3 = session.resource('s3)
+
 s3 = session.resource('s3')
-for bucket in s3.buckets.all()
-for bucket in s3.buckets.all()
-for bucket in s3.buckets.all() 
+ 
 for bucket in s3.buckets.all():
     print(bucket)
      
-new_bucket = s3.create_bucket(Bucket='automatingaws-boto3', CreateBucketConfiguration={'LocationCinstraint':'us-east-1'})
-new_bucket = s3.create_bucket(Bucket='automatingaws-boto3', CreateBucketConfiguration={'LocationConstraint':'us-east-1'})
-new_bucket = s3.create_bucket(Bucket='automatingaws-boto3', CreateBucketConfiguration={'LocationConstraint': 'us-east-1'})
-new_bucket = s3.create_bucket(Bucket='automatingaws-boto31', CreateBucketConfiguration={'LocationConstraint': 'us-east-1'})
-new_bucket = s3.create_bucket(Bucket='automatingaws-boto31', CreateBucketConfiguration={'LocationConstraint': 'us-east-1'})
 new_bucket = s3.create_bucket(Bucket="automatingaws-boto31", CreateBucketConfiguration={"LocationConstraint" : "us-east-1"})
 for bucket in s3.buckets.all():
     print(bucket)
